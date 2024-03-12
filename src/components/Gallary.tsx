131af6724f14a40c6a366f2a910b728ac035c781
PRADIP BHATT <pradip10bhatt@gmail.com>
2024-03-12 21:35:24 +0545

    error at gallary resolved

--- src/components/Gallary.tsx
+++ src/components/Gallary.tsx
@@ -8,15 +8,15 @@ export default class Gallary extends Component {
                 <h1 className='container-auto text-center w-full text-3xl mb-5 rounded-md text-black
              '>Here's my Gallary</h1>
             <div className="columns sm:columns-1 md:columns-3">
-                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="./src/assets/1.png" alt="" />
-                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="./src/assets/2.png" alt="" />
-                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="./src/assets/3.png" alt="" />
-                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="./src/assets/4.png" alt="" />
-                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="./src/assets/5.png" alt="" />
-                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="./src/assets/6.png" alt="" />
-                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="./src/assets/7.png" alt="" />
-                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="./src/assets/8.png" alt="" />
-                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="./src/assets/9.png" alt="" />
+                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="/src/assets/1.png" alt="" />
+                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="/src/assets/2.png" alt="" />
+                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="/src/assets/3.png" alt="" />
+                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="/src/assets/4.png" alt="" />
+                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="/src/assets/5.png" alt="" />
+                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="/src/assets/6.png" alt="" />
+                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="/src/assets/7.png" alt="" />
+                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="/src/assets/8.png" alt="" />
+                <img className='border p-5 hover:p-1 hover:bg-indigo-900 duration-500 rounded-full' src="/src/assets/9.png" alt="" />
             </div>
         </div>
       </div>
